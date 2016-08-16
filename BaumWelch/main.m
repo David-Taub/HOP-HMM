c956484c659e2d94e3c02b03010f0ba1ae6c0d71
Admin <Admin>
2016-08-16 15:53:48 +0300

    bet of the postirior added

--- BaumWelch/main.m
+++ BaumWelch/main.m
@@ -11,27 +11,51 @@ function maing
         
         [posE, negE] = trainMarkov(posSeqsTrain, negSeqsTrain, order);
         
-        thresholds = 0.98 : 0.00005 : 1.01;
+        % thresholds = 0.98 : 0.00005 : 1.01;
         % thresholds = 1.0004;
-        trainErrs = classify(posE, negE, posSeqsTrain, negSeqsTrain, thresholds);
-        [trainErr, i] = min(trainErrs);
-        threshold = thresholds(i);
-        testErr = classify(posE, negE, posSeqsTest, negSeqsTest, threshold);
-        [order, threshold, trainErr, testErr]
+        % trainErrs = classify(posE, negE, posSeqsTrain, negSeqsTrain, thresholds);
+        % [trainErr, i] = min(trainErrs);
+        % threshold = thresholds(i);
+        % testErr = classify(posE, negE, posSeqsTest, negSeqsTest, threshold);
+        % [order, threshold, trainErr, testErr]
 
         pos2neg = 1 / 300;
         neg2pos = 1 / 50;
         [startT, T, E] = createHmmParams(posE, negE, neg2pos, pos2neg);
 
         N = min(length(posSeqsTrain), length(negSeqsTrain));
-        N = 200;
-        posPostirior = getPostirior(posSeqsTrain, N, startT, T, E);
-        negPostirior = getPostirior(negSeqsTrain, N, startT, T, E);
+        % N = 100;
+        eta = 0.95;
+        posPostirior = getPostirior(posSeqsTrain, N, startT, T, E, eta);
+        negPostirior = getPostirior(negSeqsTrain, N, startT, T, E, eta);
 
+        
+        posTops = getTopPart(posPostirior);
+        negTops = getTopPart(negPostirior);
+
+        minTops = min(min(posTops), min(negTops));
+        maxTops = max(max(posTops), max(negTops));
+        success = [];
+        thresholds = minTops : 0.01 : maxTops;
+        for threshold = thresholds
+            correctPos = length(posTops(posTops > threshold));
+            correctNeg  = length(negTops(negTops < threshold));
+            success(end+1) = (correctPos + correctNeg) / (2 * N);
+        end
+        % best = max(success)
+        [best, i] = max(success);
+        [best, thresholds(i)]
+
+        figure 
+        hold on
+        plot(posTops)
+        plot(negTops)
+        hold off
+        legend('pos', 'neg')
         figure
         hold on;
-        plot(mean(posPostirior, 2));
-        plot(mean(negPostirior, 2));
+        plot(mean(posPostirior, 1));
+        plot(mean(negPostirior, 1));
         ylim([0,1]);
         legend('positive postirior', 'negative postirior');
         title('postirior Probability of Being Enhancer');
@@ -39,8 +63,20 @@ function maing
     end
 end
 
+function out = getTopPart(M)
+    L = size(M, 2);
+    marginsRatio = 0.15;
+    topPartRatio = 0.15;
+    M = M(:, ceil(L * marginsRatio) : end - ceil(L * marginsRatio));
+    M = sort(M, 2, 'descend');
+    M = M(:, ceil(L * marginsRatio) : end - ceil(L * marginsRatio));
+    % out = mean(M(: ,1:ceil(size(M, 2) * topPartRatio)), 2);
+    % out = sum(M>0.5, 2) / L;
+    out = mean(M, 2);
+end
 % seqs - sequences of length L
 % N - number of sequences to calculate the postirior with
+% out - N x L
 function out = getPostirior(seqs, N, startT, T, E, eta)
     m = 2;
     L = length(seqs{1});
@@ -48,20 +84,11 @@ function out = getPostirior(seqs, N, startT, T, E, eta)
     for i = 1 : N
         [alpha, scale] = forwardAlg(seqs{i}, startT, T, E, eta);
         beta = backwardAlg(seqs{i}, startT, T, E, scale, eta);
-        alphas(i, :) = alpha(1,:);
-        betas(i, :) = beta(1,:);
-        scales(i, :) = scale;
         postirior(:, :, i) = alpha .* beta ./ repmat(sum(alpha .* beta, 1), [m, 1]);
     end
-    figure;
-    hold on
-    plot(mean(alphas, 1));
-    plot(mean(betas, 1));
-    plot(mean(scales, 1));
-    hold off
-    legend('a', 'b', 's')
     % return postirior of the positive state, 
     out(:,:) = postirior(1, :, :);
+    out = out';
 end
 function [startT, T, E] = createHmmParams(posE, negE, neg2pos, pos2neg)
     E = cat(1, shiftdim(posE, -1), shiftdim(negE, -1));
