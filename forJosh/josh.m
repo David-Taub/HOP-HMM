5c8a44ac9fdd113598860c8b8f6510b22b07a1ab
David Ariel <davidariel333@gmail.com>
2016-09-28 16:34:07 +0300

    comments added

--- forJosh/josh.m
+++ forJosh/josh.m
@@ -30,19 +30,22 @@ function josh(origM)
         drawnow;
     end
 end
-
+% sites are the indices of CpG sites we use to cover the tissues
 function [covered, sites] = getSites(origM, smallGapTh, bigGapTh)
     
     pos1Peaks = [];
     neg1Peaks = [];
     covered = [];
     while true;
+        % iterations over the matrix
         C = length(covered);
+        % covered is the indices of the tissues we already "got"
         [covered, pos1PeakCur, neg1PeakCur] = ...
             get1Peaks(origM, covered, smallGapTh, bigGapTh);
         pos1Peaks = [pos1Peaks; pos1PeakCur];
         neg1Peaks = [neg1Peaks; neg1PeakCur];
         if C == length(covered) || length(covered) >= size(origM, 2) - 1
+            % no new tissues were added
             break
         end
     end
@@ -51,17 +54,25 @@ end
 
 function [covered, pos1Peaks, neg1Peaks] = get1Peaks(origM, alreadyCovered, smallGapTh, bigGapTh)
     M = origM;
+    % removed covered columns
     M(:, alreadyCovered) = [];
+
     [N,m] = size(M);
-    MSorted = sort(M, 2, 'descend');
     
+    % N x m
+    % find the interesting "peaks" lines
+    MSorted = sort(M, 2, 'descend');
+
+    % boolean vector, 1 means the line is a peak, 0 otherwise. 
+    % N x 1
     pos1Peaks = find(MSorted(:, 1) - MSorted(:, 2) > bigGapTh & ...
                     MSorted(:, 2) - MSorted(:, m) < smallGapTh);
     neg1Peaks = find(MSorted(:, m-1) - MSorted(:, m) > bigGapTh & ...
                     MSorted(:, 1) - MSorted(:, m-1) < smallGapTh);
     
     
-    % clear M from covered types
+    % n x 1, where n is the number of peaks
+    % get the tissue types of the peaks (negative and positive)
     [~, pos1PeaksType] = max(origM(pos1Peaks, :), [], 2);
     [~, neg1PeaksType] = min(origM(neg1Peaks, :), [], 2);
     covered = unique([alreadyCovered; pos1PeaksType; neg1PeaksType]);
