cebd1f33c88d5894268585a01c7e331c4e0ef397
David Ariel <davidariel333@gmail.com>
2016-09-28 16:33:00 +0300

    duplications removed

--- mm9Genome/main.m
+++ mm9Genome/main.m
@@ -1,21 +1,25 @@
 cd /cs/stud/boogalla/projects/CompGenetics/mm9Genome
-
+addpath('/cs/cbio/david/projects/CompGenetics/BaumWelch')
 fprintf('Loading Genome\n');
 load('/cs/cbio/tommy/Enhancers/Data/genome_mm9.mat');
 fprintf('Loading histone peaks\n');
 load('/cs/stud/boogalla/Work/data/mat/peaks_raw.mat');
 fprintf('Loading background sequences\n');
-negSeqs = readSeq('/cs/cbio/tommy/Enhancers/Data/NEnhancers.seq', 500);
+seqsLength = 500;
+negSeqs = readSeq('/cs/cbio/tommy/Enhancers/Data/NEnhancers.seq', seqsLength);
 fprintf('Sorting background sequences by CG-content\n');
 negSeqs = sortBaseContent(negSeqs);
 
 % work on learner:
-fprintf('Saving positive enhancers sequences\n');
-reader(T, genome, negSeqs, false);
-fprintf('Loading positive enhancers sequences\n');
-load('/cs/stud/boogalla/projects/CompGenetics/mm9Genome/data/posSeqs.mat')
-
-fprintf('Learning...\n');
-learn(posSeqs, negSeqs, overlaps); 
+    % fprintf('Saving positive enhancers sequences\n');
+    reader(T, genome, negSeqs, false, seqsLength);
+    % fprintf('Loading positive enhancers sequences\n');
+    load('/cs/stud/boogalla/projects/CompGenetics/mm9Genome/data/posSeqs.mat')
+    size(posSeqs)
+    % fprintf('Learning...\n');
+    close all; learn(posSeqs, negSeqs, overlaps); 
 % work on best dataset:
-% while 1; reader(T, genome, negSeqs, true); end;
+while 1; reader(T, genome, negSeqs, true, seqsLength); end;
+reader(T, genome, negSeqs, true, seqsLength);
+
+
--- mm9Genome/sumDim.m
+++
@@ -1,6 +0,0 @@
-function out = sumDim(M, dims)
-    out = M;
-    for i = 1 : length(dims)
-        out = sum(out, dims(i));
-    end
-end
