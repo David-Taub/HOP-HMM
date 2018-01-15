662cb0b249f5baafd2b28b98e824da5fbffc5ab6
David Ariel <davidariel333@gmail.com>
2018-01-15 16:05:37 +0100

    superEnhancer now has the sequences as well

--- src/+peaks/superEnhancerCaller.m
+++ src/+peaks/superEnhancerCaller.m
@@ -1,13 +1,33 @@
 % mergedPeaks = load('../data/peaks/mergedPeaks.mat', 'mergedPeaks');
-% peaks.superEnhancerCaller(mergedPeaks, 10000);
+% superEnhancers = peaks.superEnhancerCaller(mergedPeaks, 10000);
 % L - super enhancer size
 % mergedPeaks - enhancers data
 % finds the J regions of length L with the most enhancers starts in them
-function superEnhancerCaller(mergedPeaks, L)
-    binTicks = 1000;
-    amountToPick
+function superEnhancers = superEnhancerCaller(mergedPeaks, L)
+    close all;
+    binTicks = 2000;
+    amountToPick = 50;
+    superEnhancers = findSuperEnhancers(mergedPeaks, L, binTicks, amountToPick);
+    superEnhancers = addSequences(superEnhancers, L);
+end
+
+function superEnhancers = addSequences(superEnhancers, L)
+    dict = peaks.fasta2mem();
+    for i = 1 : length(superEnhancers.chr)
+        chr = superEnhancers.chr{i};
+        from = superEnhancers.from(i);
+        to = superEnhancers.from(i) + L - 1;
+        superEnhancers.seq{i} = dict(chr).Data(from:to);
+    end
+end
+
+function superEnhancers = findSuperEnhancers(mergedPeaks, L, binTicks, amountToPick)
+    [superEnhancers.chr{1:amountToPick}] = deal('');
+    superEnhancers.amount = zeros(1, amountToPick);
+    superEnhancers.from = zeros(1, amountToPick);
     for chrName = unique({mergedPeaks.chr})
         chrSize = getChrSize(chrName);
+        fprintf('~~%s (%d)\n', chrName{:}, chrSize);
         chrPeaks = mergedPeaks(strcmp({mergedPeaks.chr}, chrName));
         windows = zeros(1, floor(chrSize ./ binTicks));
         edges = 0 : chrSize ./ binTicks;
@@ -16,9 +36,20 @@ function superEnhancerCaller(mergedPeaks, L)
             windows = windows + chrHist;
             chrHist = [chrHist(2:end), [0]];
         end
-        figure
-        plot(chrHist)
-        drawnow;
+        while max(windows, [], 2) > min(superEnhancers.amount, [], 2)
+            [toReplaceAmount, toReplace] = min(superEnhancers.amount, [], 2);
+            [newBest, newBestfrom] = max(windows, [], 2);
+            fprintf('%s:%d (%d) <- %s:%d (%d)\n', superEnhancers.chr{toReplace}, ...
+                                                superEnhancers.from(toReplace), ...
+                                                superEnhancers.amount(toReplace), ...
+                                                chrName{:}, ...
+                                                newBestfrom * binTicks, ...
+                                                newBest)
+            superEnhancers.chr{toReplace} = chrName{:};
+            superEnhancers.amount(toReplace) = newBest;
+            superEnhancers.from(toReplace) = newBestfrom * binTicks;
+            windows(max(0,newBestfrom - floor(L / binTicks))+1:newBestfrom) = -inf;
+        end
     end
 end
 
